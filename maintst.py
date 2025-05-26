65f3a1229219ec50dc216764bd3e7acb853e92bf
Wannafreshy <Akikogeegee@gmail.com>
2025-05-26 09:50:39 +0700

    Add files via upload

--- maintst.py
+++ maintst.py
@@ -14,6 +14,23 @@ try:
         "เปลี่ยนแปลง", "เปลี่ยนแปลง(%)", "ปริมาณ(พันหุ้น)", "มูลค่า(ล้านบาท)",
         "SET Index", "SET เปลี่ยนแปลง(%)"
     ]
+    
+# แปลงวันที่ไทยเป็น YYYY-MM-DD
+    thai_months = {
+        "ม.ค.": "01", "ก.พ.": "02", "มี.ค.": "03", "เม.ย.": "04",
+        "พ.ค.": "05", "มิ.ย.": "06", "ก.ค.": "07", "ส.ค.": "08",
+        "ก.ย.": "09", "ต.ค.": "10", "พ.ย.": "11", "ธ.ค.": "12"
+    }
+
+    def convert_thai_date(thai_date_str):
+        for th, num in thai_months.items():
+            if th in thai_date_str:
+                day, month_th, year_th = thai_date_str.replace(",", "").split()
+                month = thai_months[month_th]
+                year = int(year_th) - 543
+            return f"{year}-{month}-{int(day):02d}"
+        return None
+
     df = df[~df["วันที่"].isna() & ~df["วันที่"].str.contains("วันที่")]
     df["วันที่"] = df["วันที่"].apply(convert_thai_date)
     df["วันที่"] = pd.to_datetime(df["วันที่"])
@@ -67,22 +84,6 @@ try:
 # ตั้งฟอนต์ภาษาไทย matplotlib
 # thai_font = 'Tahoma'
 # matplotlib.rcParams['font.family'] = thai_font
-
-# แปลงวันที่ไทยเป็น YYYY-MM-DD
-thai_months = {
-    "ม.ค.": "01", "ก.พ.": "02", "มี.ค.": "03", "เม.ย.": "04",
-    "พ.ค.": "05", "มิ.ย.": "06", "ก.ค.": "07", "ส.ค.": "08",
-    "ก.ย.": "09", "ต.ค.": "10", "พ.ย.": "11", "ธ.ค.": "12"
-}
-
-def convert_thai_date(thai_date_str):
-    for th, num in thai_months.items():
-        if th in thai_date_str:
-            day, month_th, year_th = thai_date_str.replace(",", "").split()
-            month = thai_months[month_th]
-            year = int(year_th) - 543
-            return f"{year}-{month}-{int(day):02d}"
-    return None
 
 # ตั้งค่าเพจ
 st.set_page_config(page_title="วิเคราะห์หุ้น TTB", layout="wide")
